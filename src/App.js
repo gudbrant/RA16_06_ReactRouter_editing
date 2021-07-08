2f558e2609a6abe7f6b3cbfec419c3782de6323d
gudbrant <61345734+gudbrant@users.noreply.github.com>
2021-07-08 13:18:45 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ServiceList from './components/ServiceList';
+import ServiceAdd from './components/ServiceAdd';
+
+
+function App() {
+  return (
+    <>
+      <ServiceAdd/>
+      <ServiceList/>
+    </>  
+  );
+}
+
+export default App;
